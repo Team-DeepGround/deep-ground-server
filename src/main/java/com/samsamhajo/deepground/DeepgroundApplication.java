ccc60b241542d3ed10e84519307d638ebd2d8a76
speculatingwook <bwook9908@gmail.com>
2025-04-18 14:26:43 +0900

    first commit

---
+++ src/main/java/com/samsamhajo/deepground/DeepgroundApplication.java
@@ -0,0 +1,13 @@
+package com.samsamhajo.deepground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepgroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsamhajo/deepground/DeepgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsamhajo.deepground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepgroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
