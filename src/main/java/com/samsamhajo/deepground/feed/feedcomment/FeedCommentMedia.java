2834b83f202e13e73e3b47c2b1041a12cab9f12b
0tae <choiyt3465@naver.com>
2025-04-28 16:14:05 +0900

    feat : 피드 댓글 미디어 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedcomment/FeedCommentMedia.java
@@ -0,0 +1,31 @@
+package com.samsamhajo.deepground.feed.feedcomment;
+
+
+import com.samsamhajo.deepground.feed.feedcomment.FeedComment;
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FeedCommentMedia extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(length = 1024)
+    public String media_url;
+
+    @Column
+    public String extension;
+
+    @ManyToOne
+    @JoinColumn(name = "feed_id")
+    public FeedComment feed;
+}
