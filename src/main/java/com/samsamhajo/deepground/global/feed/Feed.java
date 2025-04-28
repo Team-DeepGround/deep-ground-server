e253b037dc940a1c2f4118965663abb266b3a195
0tae <choiyt3465@naver.com>
2025-04-28 15:15:36 +0900

    feat : 피드 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/global/feed/Feed.java
@@ -0,0 +1,27 @@
+package com.samsamhajo.deepground.global.feed;
+
+
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feed extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(length = 4096)
+    public String content;
+
+//   TODO: Member Entity 등록 시 추가
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    public Member member;
+
+}
