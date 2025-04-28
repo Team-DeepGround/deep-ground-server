67b018cbad9ea079708bc7fe9c837de7fb128cc8
0tae <choiyt3465@naver.com>
2025-04-28 15:41:05 +0900

    feat : 피드 좋아요 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/feed/FeedLike.java
@@ -0,0 +1,29 @@
+package com.samsamhajo.deepground.feed;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FeedLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "feed_id")
+    public Feed feed;
+
+//   TODO: Member Entity 등록 시 추가
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    public Member member;
+
+}
