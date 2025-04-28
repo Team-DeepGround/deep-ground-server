923906d6b90fd1faec91aacc756fda5caf783202
0tae <choiyt3465@naver.com>
2025-04-28 15:29:49 +0900

    feat : 피드 답글 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/feedreply/FeedReply.java
@@ -0,0 +1,32 @@
+package com.samsamhajo.deepground.feedreply;
+
+
+import com.samsamhajo.deepground.feed.FeedComment;
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(length = 4096)
+    public String content;
+
+    @ManyToOne
+    @JoinColumn(name="feed_id")
+    public FeedComment feed;
+
+//   TODO: Member Entity 등록 시 추가
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    public Member member;
+
+}
