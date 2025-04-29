8acfa03c5451a3d3f6fce25a989c54309eba7e26
speculatingwook <bwook9908@gmail.com>
2025-04-30 08:29:56 +0900

    feat: StudyGroupMember entity 기본 구현
    - isAllowed는 기본값이 false

---
+++ src/main/java/com/samsamhajo/deepground/studyGroup/entity/StudyGroupMember.java
@@ -0,0 +1,27 @@
+package com.samsamhajo.deepground.studyGroup.entity;
+
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyGroupMember extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_member_id")
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "study_group_id", nullable = false)
+    private Long studyGroupId;
+
+    @Column(name = "is_allowed")
+    private Boolean isAllowed = false;
+}
