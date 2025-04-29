e3ed32191fa633f70a9abfaa54ffe0158e245f97
speculatingwook <bwook9908@gmail.com>
2025-04-30 08:45:04 +0900

    feat: StudyGroupReply entity 기본 구현

---
+++ src/main/java/com/samsamhajo/deepground/studyGroup/entity/StudyGroupReply.java
@@ -0,0 +1,27 @@
+package com.samsamhajo.deepground.studyGroup.entity;
+
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyGroupReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_group_reply_id")
+    private Long id;
+
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
