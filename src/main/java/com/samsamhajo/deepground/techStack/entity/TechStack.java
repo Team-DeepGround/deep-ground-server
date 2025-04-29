0ab2c77c24d2780e4999109c896d0206762ee2e6
jaehwannnnnn <seanjung0915@gmail.com>
2025-04-29 15:40:05 +0900

    feat: 기술 스택 관련 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/techStack/entity/TechStack.java
@@ -0,0 +1,22 @@
+package com.samsamhajo.deepground.techStack.entity;
+
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "tech_stack")
+public class TechStack extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tech_stack_id", nullable = false)
+    private Long id;
+
+    @Column(name = "tech_stack_name")
+    private String name;
+
+    @Column(name = "tech_stack_category")
+    private String category;
+}
