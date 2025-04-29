ceaa2003bb45830d5ba75baf8d5297a7d1b70ffc
paka <wjd1357912@gmail.com>
2025-04-29 16:37:58 +0900

    refactor : modules 패키지명 friend 변경,friendstatus 컬럼명 명시

--- src/main/java/com/samsamhajo/deepground/friend/Friend.java
+++ src/main/java/com/samsamhajo/deepground/friend/Friend.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 public class Friend extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = IDENTITY)
     @Column(name ="friend_id")
     private Long id;
 
