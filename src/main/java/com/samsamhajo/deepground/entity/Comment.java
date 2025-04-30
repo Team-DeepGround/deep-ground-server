31564a8c842b4d0f7b2a03f43418c8e6d4544314
dotae1 <9636515@gmail.com>
2025-04-30 12:06:50 +0900

    댓글 엔티티 구현

---
+++ src/main/java/com/samsamhajo/deepground/entity/Comment.java
@@ -0,0 +1,20 @@
+package com.samsamhajo.deepground.entity;
+
+import com.samsamhajo.deepground.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "comment_content", nullable = false)
+    private String commentContent;
+}
