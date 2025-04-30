ea546cfac6ddf35ec055192237988f9638528b3e
dotae1 <9636515@gmail.com>
2025-04-30 15:25:37 +0900

    Refactor : GenerationType -> IDENTITY questionStatus = boolean값으로 변경 colum name 설정 table name 설정

--- src/main/java/com/samsamhajo/deepground/entity/Question.java
+++ src/main/java/com/samsamhajo/deepground/entity/Question.java
@@ -7,40 +7,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table
+@Table(name = "questions")
 @NoArgsConstructor //JPA 사용을 위해 필요 (기본생성자 생성)
-@AllArgsConstructor // 필드를 다 받는 생성자
 @Getter
 public class Question extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id", nullable = false)
     private Long id;
 
-    @Column(name = "question_id", nullable = false, length = 200)
-    private String question_id;
+    @Column(name = "title", nullable = false, length = 200)
+    private String title;
 
     @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private QuestionStatus questionStatus = QuestionStatus.NOT_ACCEPTED;
+    @Column(name = "question_status",nullable = false)
+    private boolean questionStatus = false;
 
-    @Column(nullable = false) //DB에 null값이 들어가면 +1 연산할 때 문제가 발생하거나 예외 발생할 수 있어 nullable = false;
-    private int answer_count = 0;
+    @Column(name ="answer_count",nullable = false)//DB에 null값이 들어가면 +1 연산할 때 문제가 발생하거나 예외 발생할 수 있어 nullable = false;
+    private int answerCount = 0;
 
-    @Column(nullable = false)
-    private int view_count = 0;
-
-
-    //TODO : Answer, QuestionLike, QuestionMedia, QuestionTag 와 연관간계 매핑
-
-
-    //TODO : Member Entity 등록 시 추가
-    //@ManyToOne(fetch = FetchType.LAZY)
-    //@JoinColum(name = "memeber_id")
-
+    @Column(name = "view_count",nullable = false)
+    private int viewCount = 0;
 
 
 
