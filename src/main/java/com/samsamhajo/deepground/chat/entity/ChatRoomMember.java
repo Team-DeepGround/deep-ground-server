9ee945c2687ed614968d5451b8d41e73333abc50
조윤성 <yunsung1206@gmail.com>
2025-04-30 15:25:17 +0900

    refactor: ChatRoomMember 테이블 이름 지정 및 빌더 패턴 제거

--- src/main/java/com/samsamhajo/deepground/chat/entity/ChatRoomMember.java
+++ src/main/java/com/samsamhajo/deepground/chat/entity/ChatRoomMember.java
@@ -6,16 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "chat_room_members")
 public class ChatRoomMember extends BaseEntity {
 
     @Id
