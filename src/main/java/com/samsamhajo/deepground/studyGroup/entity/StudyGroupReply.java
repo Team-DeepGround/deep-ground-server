a9fa778c675b731d33af7dda5f984c4c039ba32a
speculatingwook <bwook9908@gmail.com>
2025-05-06 12:49:21 +0900

    refactor: study group reply 엔티티 생성자 생성 및 연관관계 매핑

--- src/main/java/com/samsamhajo/deepground/studyGroup/entity/StudyGroupReply.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/entity/StudyGroupReply.java
@@ -1,6 +1,7 @@
 package com.samsamhajo.deepground.studyGroup.entity;
 
 import com.samsamhajo.deepground.global.BaseEntity;
+import com.samsamhajo.deepground.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,12 +18,24 @@ public class StudyGroupReply extends BaseEntity {
     @Column(name = "study_group_reply_id")
     private Long id;
 
-    @Column(name = "comment_id", nullable = false)
-    private Long commentId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private StudyGroupComment comment;
 
-    @Column(name = "member_id", nullable = false)
-    private Long memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id", nullable = false)
+    private Member member;
 
-    @Column(name = "content", nullable = false)
+    @Column(nullable = false)
     private String content;
+
+    private StudyGroupReply(StudyGroupComment comment, Member member, String content) {
+        this.comment = comment;
+        this.member = member;
+        this.content = content;
+    }
+
+    public static StudyGroupReply of(StudyGroupComment comment, Member member, String content) {
+        return new StudyGroupReply(comment, member, content);
+    }
 }
