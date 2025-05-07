6f73e1172c974212377496d74c825f75916c28e5
조윤성 <yunsung1206@gmail.com>
2025-05-07 09:25:56 +0900

    refactor: ChatRoomMember 엔티티 연관관계 매핑

--- src/main/java/com/samsamhajo/deepground/chat/entity/ChatRoomMember.java
+++ src/main/java/com/samsamhajo/deepground/chat/entity/ChatRoomMember.java
@@ -1,11 +1,15 @@
 package com.samsamhajo.deepground.chat.entity;
 
 import com.samsamhajo.deepground.global.BaseEntity;
+import com.samsamhajo.deepground.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,11 +26,11 @@ public class ChatRoomMember extends BaseEntity {
     @Column(name = "chat_room_member_id")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id")
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "chat_room_id")
-//    private ChatRoom chatRoom;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
 }
