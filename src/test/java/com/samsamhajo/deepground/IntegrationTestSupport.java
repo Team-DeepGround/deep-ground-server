f56f342ed7d18cea61d150dcb3a8b6c01883a4c3
speculatingwook <bwook9908@gmail.com>
2025-05-09 11:31:17 +0900

    test: 테스트를 위한 기본 코드 추가
    - 통합테스트 제공을 위한 추상 클래스

---
+++ src/test/java/com/samsamhajo/deepground/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.samsamhajo.deepground;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class IntegrationTestSupport {
+}
