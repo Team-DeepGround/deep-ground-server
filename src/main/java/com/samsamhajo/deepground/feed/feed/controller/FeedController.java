1d10e5314a3e8afd87821eea5079358e63b13164
0tae <choiyt3465@naver.com>
2025-05-10 17:19:44 +0900

    feat : 피드 작성하기 API 구현

---
+++ src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedController.java
@@ -0,0 +1,28 @@
+package com.samsamhajo.deepground.feed.feed.controller;
+
+import com.samsamhajo.deepground.feed.feed.entity.Feed;
+import com.samsamhajo.deepground.feed.feed.exception.FeedSuccessCode;
+import com.samsamhajo.deepground.feed.feed.model.FeedCreateRequest;
+import com.samsamhajo.deepground.feed.feed.service.FeedService;
+import com.samsamhajo.deepground.global.success.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+    private final FeedService feedService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<SuccessResponse<Feed>> createFeed(
+            @ModelAttribute FeedCreateRequest request,
+            @RequestParam Long memberId) {
+        Feed feed = feedService.createFeed(request, memberId);
+        return ResponseEntity
+                .ok(SuccessResponse.of(FeedSuccessCode.FEED_CREATED, feed));
+    }
+}
