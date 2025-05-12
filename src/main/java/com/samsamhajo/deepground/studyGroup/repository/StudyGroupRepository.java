c502da3fb5b14534b8c2ec46f335a0956978fb35
speculatingwook <bwook9908@gmail.com>
2025-05-12 13:50:20 +0900

    feat: StudyGroupRepository 추가

---
+++ src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupRepository.java
@@ -0,0 +1,7 @@
+package com.samsamhajo.deepground.studyGroup.repository;
+
+import com.samsamhajo.deepground.studyGroup.entity.StudyGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
+}
