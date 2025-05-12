81fda9dbf6c1003a081a2fb3363df8a177a41297
paka <wjd1357912@gmail.com>
2025-05-12 12:15:33 +0900

    feat : ResponseDto 및 친구 요청 성공 메세지 추가

--- src/main/java/com/samsamhajo/deepground/friend/service/FriendService.java
+++ src/main/java/com/samsamhajo/deepground/friend/service/FriendService.java
@@ -34,12 +34,9 @@ public class FriendService {
         if(requester.getEmail().equals(receiver.getEmail())) {
             throw new FriendException(FriendErrorCode.SELF_REQUEST);
         }
-        if(memberRepository.existsByRequestMemberAndReceiveMember(requester,receiver)) {
+        if(friendRepository.existsByRequestMemberAndReceiveMember(requester,receiver)) {
             throw new FriendException(FriendErrorCode.ALREADY_FRIEND);
         }
-
-
-
 
         Friend friend = Friend.request(requester, receiver);
         friendRepository.save(friend);
