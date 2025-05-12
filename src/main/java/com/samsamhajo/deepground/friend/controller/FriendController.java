ccf49b84a781a6e5e251a617d90c8dd296cb750a
paka <wjd1357912@gmail.com>
2025-05-12 23:59:50 +0900

    feat : FriendDTO 및 친구 목록 컨트롤러 작성

--- src/main/java/com/samsamhajo/deepground/friend/controller/FriendController.java
+++ src/main/java/com/samsamhajo/deepground/friend/controller/FriendController.java
@@ -1,5 +1,6 @@
 package com.samsamhajo.deepground.friend.controller;
 
+import com.samsamhajo.deepground.friend.Dto.FriendDto;
 import com.samsamhajo.deepground.friend.Dto.FriendRequestDto;
 import com.samsamhajo.deepground.friend.Dto.ResponseDto;
 import com.samsamhajo.deepground.friend.service.FriendService;
@@ -10,30 +11,32 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
-
-@RestController
+@RestController("/api/v1/friends")
 @RequiredArgsConstructor
 public class FriendController {
 
     private final FriendService friendService;
 
-    @PostMapping("/api/v1/friends/request")
+    @PostMapping("/request")
     public ResponseEntity<ResponseDto> requestFriend(@RequestBody @Valid FriendRequestDto dto) {
         Long friendId = friendService.sendFriendRequest(dto.getRequesterId(), dto.getReceiverEmail());
         return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "친구 요청을 보냈습니다!", new SendFriendResponse(friendId)));
     }
 
-
-
     @Data
     @AllArgsConstructor
     static class SendFriendResponse {
         private Long id;
+    }
+
+    @GetMapping("/sent")
+    public ResponseEntity<List<FriendDto>> getSentFriendRequests(@RequestParam Long requesterId) {
+        return ResponseEntity.ok(friendService.findSentFriendRequest(requesterId));
     }
 
 }
