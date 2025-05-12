33791af1c87396be27f38233ac9548f7d7653ef8
speculatingwook <bwook9908@gmail.com>
2025-05-12 16:41:26 +0900

    feat: 스터디 그룹 조회 controller 구현

---
+++ src/main/java/com/samsamhajo/deepground/studyGroup/controller/StudyGroupController.java
@@ -0,0 +1,31 @@
+package com.samsamhajo.deepground.studyGroup.controller;
+
+import com.samsamhajo.deepground.global.success.SuccessResponse;
+import com.samsamhajo.deepground.global.utils.GlobalLogger;
+import com.samsamhajo.deepground.studyGroup.dto.StudyGroupSearchRequest;
+import com.samsamhajo.deepground.studyGroup.service.StudyGroupService;
+import com.samsamhajo.deepground.studyGroup.success.StudyGroupSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/study-group")
+public class StudyGroupController {
+
+  private final StudyGroupService studyGroupService;
+
+  @GetMapping("/search")
+  public ResponseEntity<SuccessResponse<?>> searchStudyGroups(
+      @ModelAttribute StudyGroupSearchRequest request
+  ) {
+    GlobalLogger.info("스터디 목록 검색 요청", request.getKeyword(), request.getGroupStatus());
+
+    var response = studyGroupService.searchStudyGroups(request);
+
+    return ResponseEntity
+        .status(StudyGroupSuccessCode.CREATE_SUCCESS.getStatus())
+        .body(SuccessResponse.of(StudyGroupSuccessCode.CREATE_SUCCESS, response));
+  }
+}
