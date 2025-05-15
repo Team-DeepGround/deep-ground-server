a1aab3d75a8bcaa70ed80b0a155bcff7ef98a318
0tae <choiyt3465@naver.com>
2025-05-15 18:08:09 +0900

    fix : 응답을 이미지만 반환하도록 수정

--- src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
@@ -2,10 +2,9 @@ package com.samsamhajo.deepground.feed.feed.controller;
 
 import com.samsamhajo.deepground.feed.feed.model.FeedMediaResponse;
 import com.samsamhajo.deepground.feed.feed.service.FeedMediaService;
-import com.samsamhajo.deepground.global.success.SuccessResponse;
-import com.samsamhajo.deepground.media.MediaSuccessCode;
 import com.samsamhajo.deepground.media.MediaUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,12 +21,12 @@ public class FeedMediaController {
 
     // 피드 이미지 아이디로 이미지 불러오기
     @GetMapping("/{mediaId}")
-    public ResponseEntity<SuccessResponse<FeedMediaResponse>> viewImage(@PathVariable("mediaId") Long mediaId) {
+    public ResponseEntity<InputStreamResource> viewImage(@PathVariable("mediaId") Long mediaId) {
         FeedMediaResponse feedMediaResponse =
                 feedMediaService.fetchFeedMedia(mediaId);
 
         return ResponseEntity.ok()
                 .contentType(MediaUtils.getMediaType(feedMediaResponse.getExtension()))
-                .body(SuccessResponse.of(MediaSuccessCode.MEDIA_FOUND, feedMediaResponse));
+                .body(feedMediaResponse.getImage());
     }
 }
