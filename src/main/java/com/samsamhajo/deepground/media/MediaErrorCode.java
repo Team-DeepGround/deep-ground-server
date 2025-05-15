57f930bb43705c0492c9ae259e0ddd48dff5fc09
0tae <choiyt3465@naver.com>
2025-05-15 17:19:51 +0900

    feat : 미디어 에러코드 추가 - 지원하지 않는 미디어 타입, 저장 실패 에러코드 추가

--- src/main/java/com/samsamhajo/deepground/media/MediaErrorCode.java
+++ src/main/java/com/samsamhajo/deepground/media/MediaErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum MediaErrorCode implements ErrorCode {
     MEDIA_NOT_FOUND(HttpStatus.NOT_FOUND, "미디어를 찾을 수 없습니다."),
-
+    MEDIA_NOT_SUPPORTED(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "지원하지 않는 미디어 타입입니다."),
+    MEDIA_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "미디어 저장에 실패했습니다."),
     ;
 
     private final HttpStatus status;
