8ff9c0c34fd344907776ab1b1d8435c8dd3a1938
jaehwannnnnn <seanjung0915@gmail.com>
2025-05-15 13:39:10 +0900

    refactor: validator 삭제 및 중복 검사 로직 service에 추가

--- src/main/java/com/samsamhajo/deepground/auth/service/AuthService.java
+++ src/main/java/com/samsamhajo/deepground/auth/service/AuthService.java
@@ -1,7 +1,8 @@
 package com.samsamhajo.deepground.auth.service;
 
 import com.samsamhajo.deepground.auth.dto.RegisterRequest;
-import com.samsamhajo.deepground.auth.validator.RegisterValidator;
+import com.samsamhajo.deepground.auth.exception.AuthErrorCode;
+import com.samsamhajo.deepground.auth.exception.AuthException;
 import com.samsamhajo.deepground.member.entity.Member;
 import com.samsamhajo.deepground.member.repository.MemberRepository;
 import jakarta.transaction.Transactional;
@@ -14,14 +15,13 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final RegisterValidator registerValidator;
 
     @Transactional
     public Long register(RegisterRequest request) {
 
         // 중복 검사
-        registerValidator.validateEmail(request.getEmail());
-        registerValidator.validateNickname(request.getNickname());
+        checkEmailDuplicate(request.getEmail());
+        checkNicknameDuplicate(request.getNickname());
 
         Member member = Member.createLocalMember(
                 request.getEmail(),
@@ -31,5 +31,17 @@ public class AuthService {
 
         Member savedMember = memberRepository.save(member);
         return savedMember.getId();
+    }
+
+    public void checkEmailDuplicate(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new AuthException(AuthErrorCode.DUPLICATE_EMAIL);
+        }
+    }
+
+    public void checkNicknameDuplicate(String nickname) {
+        if (memberRepository.existsByNickname(nickname)) {
+            throw new AuthException(AuthErrorCode.DUPLICATE_NICKNAME);
+        }
     }
 }
