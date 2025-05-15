4504b5534eafa4c36cdc392b36a6558770f80881
0tae <choiyt3465@naver.com>
2025-05-15 17:08:10 +0900

    fix : 미디어 조회하기 요청 경로 및 성공 코드 수정 - /media/media -> /media - FeedSuccessCode -> MediaSuccessCode

--- src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
@@ -1,9 +1,9 @@
 package com.samsamhajo.deepground.feed.feed.controller;
 
-import com.samsamhajo.deepground.feed.feed.exception.FeedSuccessCode;
 import com.samsamhajo.deepground.feed.feed.model.FeedMediaResponse;
 import com.samsamhajo.deepground.feed.feed.service.FeedMediaService;
 import com.samsamhajo.deepground.global.success.SuccessResponse;
+import com.samsamhajo.deepground.media.MediaSuccessCode;
 import com.samsamhajo.deepground.media.MediaUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +20,14 @@ public class FeedMediaController {
     private final FeedMediaService feedMediaService;
 
 
-    // TODO: 피드 이미지 아이디로 이미지 불러오기 API 문서에 추가(하나씩 요청해서 불러올것)
-    @GetMapping("/media/{mediaId}")
+    // 피드 이미지 아이디로 이미지 불러오기
+    @GetMapping("/{mediaId}")
     public ResponseEntity<SuccessResponse<FeedMediaResponse>> viewImage(@PathVariable("mediaId") Long mediaId) {
         FeedMediaResponse feedMediaResponse =
                 feedMediaService.fetchFeedMedia(mediaId);
 
         return ResponseEntity.ok()
                 .contentType(MediaUtils.getMediaType(feedMediaResponse.getExtension()))
-                .body(SuccessResponse.of(FeedSuccessCode.FEEDS_RETRIEVED, feedMediaResponse));
+                .body(SuccessResponse.of(MediaSuccessCode.MEDIA_FOUND, feedMediaResponse));
     }
 }
