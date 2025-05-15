9d83d533824725544c1efa65ac36785f529d6e7c
jaehwannnnnn <seanjung0915@gmail.com>
2025-05-15 16:51:35 +0900

    chore: 오타 수정

--- src/main/java/com/samsamhajo/deepground/interest/repository/MemberInterestRepository.java
+++ src/main/java/com/samsamhajo/deepground/interest/repository/MemberInterestRepository.java
@@ -1,9 +1,9 @@
 package com.samsamhajo.deepground.interest.repository;
 
-import com.samsamhajo.deepground.member.repository.MemberRepository;
+import com.samsamhajo.deepground.interest.entity.MemberInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberInterestRepository extends JpaRepository<MemberRepository, Long> {
+public interface MemberInterestRepository extends JpaRepository<MemberInterest, Long> {
 }
