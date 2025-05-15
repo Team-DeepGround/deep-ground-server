1a16e235ca8a6131ec72a10c528564974115b297
0tae <choiyt3465@naver.com>
2025-05-15 20:02:41 +0900

    fix : /api/v1 제거

--- src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/controller/FeedMediaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/feed/media")
+@RequestMapping("/feed/media")
 @RequiredArgsConstructor
 public class FeedMediaController {
 
