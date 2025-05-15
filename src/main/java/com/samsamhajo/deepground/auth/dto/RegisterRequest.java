876d32cfc07427f28076b356f817fcd170841cee
jaehwannnnnn <seanjung0915@gmail.com>
2025-05-15 14:21:07 +0900

    Test: 회원가입 테스트 코드 작성

--- src/main/java/com/samsamhajo/deepground/auth/dto/RegisterRequest.java
+++ src/main/java/com/samsamhajo/deepground/auth/dto/RegisterRequest.java
@@ -3,11 +3,13 @@ package com.samsamhajo.deepground.auth.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class RegisterRequest {
 
     @NotBlank
