e47dc8e5fc06f1f770a35bd17d07e3a46470f08b
paka <wjd1357912@gmail.com>
2025-05-16 17:15:10 +0900

    refactor : ResponseDto 삭제 및 FirendSuccessCode Enum클래스로 정의 후 응답 표준 클래스 활용

--- src/main/java/com/samsamhajo/deepground/friend/controller/FriendController.java
+++ src/main/java/com/samsamhajo/deepground/friend/controller/FriendController.java
@@ -3,13 +3,13 @@ package com.samsamhajo.deepground.friend.controller;
 
 import com.samsamhajo.deepground.friend.Dto.FriendDto;
 import com.samsamhajo.deepground.friend.Dto.FriendRequestDto;
-import com.samsamhajo.deepground.friend.Dto.ResponseDto;
+import com.samsamhajo.deepground.friend.Exception.FriendSuccessCode;
 import com.samsamhajo.deepground.friend.service.FriendService;
+import com.samsamhajo.deepground.global.success.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,16 +24,12 @@ public class FriendController {
     private final FriendService friendService;
 
     @PostMapping("/request")
-    public ResponseEntity<ResponseDto> requestFriend(@RequestBody @Valid FriendRequestDto dto) {
+    public ResponseEntity<SuccessResponse> requestFriend(@RequestBody @Valid FriendRequestDto dto) {
         Long friendId = friendService.sendFriendRequest(dto.getRequesterId(), dto.getReceiverEmail());
-        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "친구 요청을 보냈습니다!", new SendFriendResponse(friendId)));
+        return ResponseEntity
+                        .ok(SuccessResponse.of(FriendSuccessCode.FRIEND_SUCCESS_REQUEST,friendId));
     }
 
-    @Data
-    @AllArgsConstructor
-    static class SendFriendResponse  {
-        private Long id;
-    }
 
     @GetMapping("/sent")
     public ResponseEntity<List<FriendDto>> getSentFriendRequests(@RequestParam Long requesterId) {
@@ -42,10 +38,11 @@ public class FriendController {
 
 
     @PatchMapping("/sent/{friendId}/cancel")
-    public ResponseEntity<ResponseDto> cancelFriendRequest(@PathVariable Long friendId,
+    public ResponseEntity<SuccessResponse> cancelFriendRequest(@PathVariable Long friendId,
                                                            @RequestParam Long requesterId){
         friendService.cancelFriendRequest(friendId,requesterId);
-        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "친구 요청 취소가 되었습니다!",friendId));
+        return ResponseEntity
+                .ok(SuccessResponse.of(FriendSuccessCode.FRIEND_SUCCESS_CANCEL,friendId));
 
     }
 
