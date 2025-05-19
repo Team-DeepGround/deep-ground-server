89f6c5e5d375568c4d52b1923eb5b72415a3b4fe
speculatingwook <bwook9908@gmail.com>
2025-05-19 14:00:23 +0900

    feat: 스터디그룹 참가 요청 기능 구현 - 프레젠테이션 레이어

--- src/main/java/com/samsamhajo/deepground/studyGroup/success/StudyGroupSuccessCode.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/success/StudyGroupSuccessCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum StudyGroupSuccessCode implements SuccessCode {
   CREATE_SUCCESS(HttpStatus.CREATED, "스터디 그룹이 성공적으로 생성되었습니다."),
   READ_SUCCESS(HttpStatus.OK, "스터디 그룹 상세 조회 성공"),
-  SEARCH_SUCCESS(HttpStatus.FOUND, "스터디 그룹 조회 성공");
+  SEARCH_SUCCESS(HttpStatus.FOUND, "스터디 그룹 조회 성공"),
+  REQUEST_JOIN_SUCCESS(HttpStatus.OK, "스터디 참가 요청이 완료되었습니다.");
 
   private final HttpStatus status;
   private final String message;
