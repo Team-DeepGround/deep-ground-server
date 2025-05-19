414c8f38fc0c2797916b57b0b84d2d91e3b941e3
speculatingwook <bwook9908@gmail.com>
2025-05-19 13:41:03 +0900

    feat: 스터디그룹에 있는 멤버 찾는 기능 구현
    - 레포지토리단

--- src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupMemberRepository.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupMemberRepository.java
@@ -1,9 +1,15 @@
 package com.samsamhajo.deepground.studyGroup.repository;
 
+import com.samsamhajo.deepground.member.entity.Member;
+import com.samsamhajo.deepground.studyGroup.entity.StudyGroup;
 import com.samsamhajo.deepground.studyGroup.entity.StudyGroupMember;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyGroupMemberRepository extends JpaRepository<StudyGroupMember, Long> {
+  boolean existsByMemberAndStudyGroup(Member member, StudyGroup studyGroup);
+
+  Optional<StudyGroupMember> findByMemberAndStudyGroup(Member member, StudyGroup studyGroup);
 }
