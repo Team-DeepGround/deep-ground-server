689be3eb407d0b8ccfcaf7497f9969ee88f21809
조윤성 <yunsung1206@gmail.com>
2025-05-22 21:41:27 +0900

    refactor: 채팅 메시지 전송 기능에 트랜잭션 적용

--- src/main/java/com/samsamhajo/deepground/chat/service/ChatMessageService.java
+++ src/main/java/com/samsamhajo/deepground/chat/service/ChatMessageService.java
@@ -7,9 +7,9 @@ import com.samsamhajo.deepground.chat.exception.ChatMessageErrorCode;
 import com.samsamhajo.deepground.chat.exception.ChatMessageException;
 import com.samsamhajo.deepground.chat.repository.ChatMessageRepository;
 import com.samsamhajo.deepground.global.message.MessagePublisher;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
@@ -19,8 +19,8 @@ public class ChatMessageService {
 
     private final MessagePublisher messagePublisher;
     private final ChatMessageRepository chatMessageRepository;
-    private final ChatRedisService chatRedisService;
 
+    @Transactional
     public void sendMessage(Long chatRoomId, Long memberId, ChatMessageRequest request) {
         if (!StringUtils.hasText(request.getMessage()) && CollectionUtils.isEmpty(request.getMediaIds())) {
             throw new ChatMessageException(ChatMessageErrorCode.INVALID_MESSAGE);
@@ -30,10 +30,6 @@ public class ChatMessageService {
 
         ChatMessage chatMessage = ChatMessage.of(chatRoomId, memberId, request.getMessage());
         chatMessageRepository.save(chatMessage);
-
-        // 가장 최근 메시지 시간 업데이트
-        LocalDateTime latestMessageTime = chatMessage.getCreatedAt();
-        chatRedisService.updateLatestMessageTime(chatRoomId, latestMessageTime);
 
         // 채팅 메시지 전송
         String destination = "/chatrooms/" + chatRoomId + "/message";
