bf1124b2824f48deff7a78e90f3f41201f279cc9
dotae1 <9636515@gmail.com>
2025-05-22 16:35:13 +0900

    수정사항 반영2

--- src/main/java/com/samsamhajo/deepground/qna/question/Dto/QuestionUpdateDto.java
+++ src/main/java/com/samsamhajo/deepground/qna/question/Dto/QuestionUpdateDto.java
@@ -1,5 +1,6 @@
 package com.samsamhajo.deepground.qna.question.Dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +17,9 @@ public class QuestionUpdateDto {
     private Long questionId;
 
     @Size(max = 100, message = "제목은 100자 이내로 입력해야 합니다.")
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
+    @NotBlank(message = "내용은 필수입니다.")
     private String content;
     private List<Long> techStacks;
     private List<MultipartFile> images;
