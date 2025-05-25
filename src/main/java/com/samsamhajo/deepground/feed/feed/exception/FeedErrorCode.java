ce7c08562fc8acf6b9539bb75b3629f5e18ab471
0tae <choiyt3465@naver.com>
2025-05-25 16:11:59 +0900

    feat : 피드 좋아요 관련 에러 및 성공 코드 정의

--- src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedErrorCode.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedErrorCode.java
@@ -12,6 +12,9 @@ public enum FeedErrorCode implements ErrorCode {
     FEED_DELETE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "피드 삭제 권한이 없습니다."),
     FEED_MEDIA_NOT_FOUND(HttpStatus.NOT_FOUND, "피드 미디어를 찾을 수 없습니다."),
 
+    FEED_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 피드 및 좋아요를 찾을 수 없습니다."),
+    FEED_LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 좋아요가 눌러져 있습니다."),
+
     ;
 
     private final HttpStatus status;
--- src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedSuccessCode.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedSuccessCode.java
@@ -10,7 +10,9 @@ public enum FeedSuccessCode implements SuccessCode {
     FEEDS_RETRIEVED(HttpStatus.OK, "피드 목록이 성공적으로 조회되었습니다."),
     FEED_UPDATED(HttpStatus.OK, "피드가 성공적으로 수정되었습니다."),
     FEED_DELETED(HttpStatus.OK, "피드가 성공적으로 삭제되었습니다."),
-    FEED_MEDIA_UPDATED(HttpStatus.OK, "피드 미디어가 성공적으로 수정되었습니다.")
+    FEED_MEDIA_UPDATED(HttpStatus.OK, "피드 미디어가 성공적으로 수정되었습니다."),
+    FEED_LIKED(HttpStatus.OK, "피드가 성공적으로 좋아요 되었습니다."),
+    FEED_UNLIKED(HttpStatus.OK, "피드 좋아요가 취소되었습니다."),
 
     ;
 
