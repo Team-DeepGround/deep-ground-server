1ac4f9652628907b3014ba7c8623c89d1c17aad0
조윤성 <yunsung1206@gmail.com>
2025-05-29 01:30:54 +0900

    refactor: 알림 목록 조회 요청 파라미터 개선

--- src/main/java/com/samsamhajo/deepground/notification/controller/NotificationController.java
+++ src/main/java/com/samsamhajo/deepground/notification/controller/NotificationController.java
@@ -2,17 +2,17 @@ package com.samsamhajo.deepground.notification.controller;
 
 import com.samsamhajo.deepground.auth.security.CustomUserDetails;
 import com.samsamhajo.deepground.global.success.SuccessResponse;
+import com.samsamhajo.deepground.notification.dto.NotificationListRequest;
 import com.samsamhajo.deepground.notification.dto.NotificationListResponse;
 import com.samsamhajo.deepground.notification.service.NotificationService;
 import com.samsamhajo.deepground.notification.success.NotificationSuccessCode;
-import java.time.LocalDateTime;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,13 +24,16 @@ public class NotificationController {
 
     @GetMapping
     public ResponseEntity<SuccessResponse<NotificationListResponse>> getNotifications(
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime cursor,
-            @RequestParam(defaultValue = "10") int limit,
+            @Valid @ModelAttribute NotificationListRequest request,
             @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
         Long memberId = userDetails.getMember().getId();
 
-        NotificationListResponse notifications = notificationService.getNotifications(memberId, cursor, limit);
+        NotificationListResponse notifications = notificationService.getNotifications(
+                memberId,
+                request.getCursor(),
+                request.getLimit()
+        );
         return ResponseEntity
                 .ok(SuccessResponse.of(NotificationSuccessCode.NOTIFICATION_RETRIEVED, notifications));
     }
