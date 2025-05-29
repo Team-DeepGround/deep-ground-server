d018fff096228e4be0f1397e8c704e19e2291a72
ynyejin <yyj030625@gmail.com>
2025-05-29 16:14:41 +0900

    feat: MemberStudyScheduleRequestDto에 유효성 검증 추가 및 컨트롤러 Valid 적용

--- src/main/java/com/samsamhajo/deepground/calendar/controller/MemberStudyScheduleController.java
+++ src/main/java/com/samsamhajo/deepground/calendar/controller/MemberStudyScheduleController.java
@@ -5,6 +5,7 @@ import com.samsamhajo.deepground.calendar.dto.MemberStudyScheduleResponseDto;
 import com.samsamhajo.deepground.calendar.exception.ScheduleSuccessCode;
 import com.samsamhajo.deepground.calendar.service.MemberStudyScheduleService;
 import com.samsamhajo.deepground.global.success.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class MemberStudyScheduleController {
     @PatchMapping("/{memberStudyScheduleId}")
     public ResponseEntity<SuccessResponse<MemberStudyScheduleResponseDto>> update(
             @PathVariable Long memberStudyScheduleId,
-            @RequestBody MemberStudyScheduleRequestDto requestDto
+            @Valid @RequestBody MemberStudyScheduleRequestDto requestDto
     ) {
         MemberStudyScheduleResponseDto responseDto = memberStudyScheduleService.update(memberStudyScheduleId, requestDto);
         return ResponseEntity.status(ScheduleSuccessCode.SCHEDULE_UPDATED.getStatus())
