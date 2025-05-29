ed1f360766c766b18b09b87b176e862aa308061a
0tae <choiyt3465@naver.com>
2025-05-29 14:16:45 +0900

    refactor : 미사용 에러코드 제거 및 status 변경 - FEED_LIKE_MINUS_NOT_ALLOWED의 status를 BAD_REQUEST로 변경

--- src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedErrorCode.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/exception/FeedErrorCode.java
@@ -8,12 +8,10 @@ import org.springframework.http.HttpStatus;
 public enum FeedErrorCode implements ErrorCode {
     FEED_NOT_FOUND(HttpStatus.NOT_FOUND, "피드를 찾을 수 없습니다."),
     INVALID_FEED_CONTENT(HttpStatus.BAD_REQUEST, "피드 내용이 유효하지 않습니다."),
-    FEED_UPDATE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "피드 수정 권한이 없습니다."),
-    FEED_DELETE_PERMISSION_DENIED(HttpStatus.FORBIDDEN, "피드 삭제 권한이 없습니다."),
     FEED_MEDIA_NOT_FOUND(HttpStatus.NOT_FOUND, "피드 미디어를 찾을 수 없습니다."),
 
     FEED_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 피드 및 좋아요를 찾을 수 없습니다."),
-    FEED_LIKE_MINUS_NOT_ALLOWED(HttpStatus.FORBIDDEN, "좋아요는 음수가 될 수 없습니다."),
+    FEED_LIKE_MINUS_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "좋아요는 음수가 될 수 없습니다."),
     FEED_LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 좋아요가 눌러져 있습니다."),
 
     ;
