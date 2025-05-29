88d1de37205c647ac80c5ebb8830a02d8d3914fb
조윤성 <yunsung1206@gmail.com>
2025-05-29 10:15:21 +0900

    test: 알림 목록 조회 기능 테스트 코드 작성

--- src/test/java/com/samsamhajo/deepground/notification/service/NotificationServiceTest.java
+++ src/test/java/com/samsamhajo/deepground/notification/service/NotificationServiceTest.java
@@ -5,17 +5,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.samsamhajo.deepground.global.message.MessagePublisher;
 import com.samsamhajo.deepground.member.entity.Member;
+import com.samsamhajo.deepground.notification.dto.NotificationListResponse;
 import com.samsamhajo.deepground.notification.dto.NotificationResponse;
 import com.samsamhajo.deepground.notification.entity.Notification;
 import com.samsamhajo.deepground.notification.entity.NotificationData;
 import com.samsamhajo.deepground.notification.entity.data.FriendNotificationData;
 import com.samsamhajo.deepground.notification.repository.NotificationDataRepository;
 import com.samsamhajo.deepground.notification.repository.NotificationRepository;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -64,5 +69,59 @@ class NotificationServiceTest {
                 eq("/notifications"),
                 any(NotificationResponse.class)
         );
+    }
+
+    @Nested
+    @DisplayName("알림 목록 조회")
+    public class GetNotification {
+
+        private final Long receiverId = 1L;
+        private final LocalDateTime cursor = LocalDateTime.now();
+        private final int limit = 10;
+
+        @Test
+        @DisplayName("다음 페이지가 존재한다")
+        void getNotifications_hasNextTrue_success() {
+            // given
+            List<Notification> notifications = IntStream.range(0, limit + 1)
+                    .mapToObj(i -> mock(Notification.class))
+                    .toList();
+
+            when(notifications.get(notifications.size() - 2).getCreatedAt())
+                    .thenReturn(cursor);
+            when(notificationRepository.findByReceiverIdWithCursor(eq(receiverId), eq(cursor), eq(limit)))
+                    .thenReturn(notifications);
+
+            // when
+            NotificationListResponse response = notificationService.getNotifications(receiverId, cursor, limit);
+
+            // then
+            assertThat(response.getNotifications()).hasSize(limit);
+            assertThat(response.isHasNext()).isTrue();
+            assertThat(response.getNextCursor()).isNotNull();
+        }
+
+        @Test
+        @DisplayName("다음 페이지가 존재하지 않는다")
+        void getNotifications_hasNextFalse_success() {
+            // given
+            int size = 5;
+            List<Notification> notifications = IntStream.range(0, size)
+                    .mapToObj(i -> mock(Notification.class))
+                    .toList();
+
+            when(notifications.get(size - 1).getCreatedAt())
+                    .thenReturn(cursor);
+            when(notificationRepository.findByReceiverIdWithCursor(eq(receiverId), eq(cursor), eq(limit)))
+                    .thenReturn(notifications);
+
+            // when
+            NotificationListResponse response = notificationService.getNotifications(receiverId, cursor, limit);
+
+            // then
+            assertThat(response.getNotifications()).hasSize(size);
+            assertThat(response.isHasNext()).isFalse();
+            assertThat(response.getNextCursor()).isNotNull();
+        }
     }
 }
