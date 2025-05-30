c824899d2e1146dc5b5265bd357569e6cf68ed68
0tae <choiyt3465@naver.com>
2025-05-30 17:24:09 +0900

    feat : 피드 댓글 생성 로직 구현 - 모든 이미지에 대해 feedComment 및 imageUrl, extention으로 구성하여 저장

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedcomment/service/FeedCommentService.java
@@ -0,0 +1,51 @@
+package com.samsamhajo.deepground.feed.feedcomment.service;
+
+import com.samsamhajo.deepground.feed.feed.entity.Feed;
+import com.samsamhajo.deepground.feed.feed.repository.FeedRepository;
+import com.samsamhajo.deepground.feed.feedcomment.entity.FeedComment;
+import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentErrorCode;
+import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentException;
+import com.samsamhajo.deepground.feed.feedcomment.model.FeedCommentCreateRequest;
+import com.samsamhajo.deepground.feed.feedcomment.repository.FeedCommentRepository;
+import com.samsamhajo.deepground.member.entity.Member;
+import com.samsamhajo.deepground.member.exception.MemberErrorCode;
+import com.samsamhajo.deepground.member.exception.MemberException;
+import com.samsamhajo.deepground.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FeedCommentService {
+
+    private final FeedCommentRepository feedCommentRepository;
+    private final FeedCommentMediaService feedCommentMediaService;
+    private final FeedRepository feedRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public FeedComment createFeedComment(FeedCommentCreateRequest request, Long memberId) {
+        if (!StringUtils.hasText(request.getContent())) {
+            throw new FeedCommentException(FeedCommentErrorCode.INVALID_FEED_COMMENT_CONTENT);
+        }
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_MEMBER_ID));
+        
+        Feed feed = feedRepository.getById(request.getFeedId());
+        
+        FeedComment feedComment = FeedComment.of(request.getContent(), feed, member);
+        feedCommentRepository.save(feedComment);
+
+        saveFeedCommentMedia(request, feedComment);
+
+        return feedComment;
+    }
+
+    private void saveFeedCommentMedia(FeedCommentCreateRequest request, FeedComment feedComment) {
+        feedCommentMediaService.createFeedCommentMedia(feedComment, request.getImages());
+    }
+} 
