926a31e9425187938201f7e27c0e44de65db779c
jaehwannnnnn <seanjung0915@gmail.com>
2025-05-30 11:28:16 +0900

    hotfix: AuthErrorCode, AuthSuccessCode 관련 충돌 해결

--- src/main/java/com/samsamhajo/deepground/auth/exception/AuthErrorCode.java
+++ src/main/java/com/samsamhajo/deepground/auth/exception/AuthErrorCode.java
@@ -16,7 +16,7 @@ public enum AuthErrorCode implements ErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "잘못된 인증 코드입니다."),
-    VERIFICATION_CODE_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 인증 코드입니다.");
+    VERIFICATION_CODE_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 인증 코드입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다.");
 
