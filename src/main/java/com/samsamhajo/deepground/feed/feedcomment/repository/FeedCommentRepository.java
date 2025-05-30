77d530a401c72e2d7a5692db474788eb92ffc1bd
0tae <choiyt3465@naver.com>
2025-05-30 17:19:09 +0900

    feat : 피드 댓글 레포지토리 구현 - getById 구현

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedcomment/repository/FeedCommentRepository.java
@@ -0,0 +1,14 @@
+package com.samsamhajo.deepground.feed.feedcomment.repository;
+
+import com.samsamhajo.deepground.feed.feedcomment.entity.FeedComment;
+import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentErrorCode;
+import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedCommentRepository extends JpaRepository<FeedComment, Long> {
+    default FeedComment getById(Long feedCommentId) {
+        return findById(feedCommentId).orElseThrow(() -> new FeedCommentException(FeedCommentErrorCode.FEED_COMMENT_NOT_FOUND));
+    }
+} 
