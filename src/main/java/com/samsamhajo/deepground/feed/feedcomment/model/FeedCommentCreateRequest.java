70045a45192c12f780aed253b2c81d5da9b0343b
0tae <choiyt3465@naver.com>
2025-05-31 15:25:58 +0900

    feat :AllArgsConstructor Lombok 어노테이션 추가

--- src/main/java/com/samsamhajo/deepground/feed/feedcomment/model/FeedCommentCreateRequest.java
+++ src/main/java/com/samsamhajo/deepground/feed/feedcomment/model/FeedCommentCreateRequest.java
@@ -1,11 +1,13 @@
 package com.samsamhajo.deepground.feed.feedcomment.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class FeedCommentCreateRequest {
     private Long feedId;
     private String content;
