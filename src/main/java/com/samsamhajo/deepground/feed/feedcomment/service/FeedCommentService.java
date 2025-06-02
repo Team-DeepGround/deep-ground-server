c791af65850aa311b0d73ac09324c7de5e55f911
0tae <choiyt3465@naver.com>
2025-06-02 15:27:55 +0900

    feat : 피드 댓글 수정 서비스 로직 구현

--- src/main/java/com/samsamhajo/deepground/feed/feedcomment/service/FeedCommentService.java
+++ src/main/java/com/samsamhajo/deepground/feed/feedcomment/service/FeedCommentService.java
@@ -6,6 +6,7 @@ import com.samsamhajo.deepground.feed.feedcomment.entity.FeedComment;
 import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentErrorCode;
 import com.samsamhajo.deepground.feed.feedcomment.exception.FeedCommentException;
 import com.samsamhajo.deepground.feed.feedcomment.model.FeedCommentCreateRequest;
+import com.samsamhajo.deepground.feed.feedcomment.model.FeedCommentUpdateRequest;
 import com.samsamhajo.deepground.feed.feedcomment.repository.FeedCommentRepository;
 import com.samsamhajo.deepground.member.entity.Member;
 import com.samsamhajo.deepground.member.exception.MemberErrorCode;
@@ -34,13 +35,30 @@ public class FeedCommentService {
 
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_MEMBER_ID));
-        
+
         Feed feed = feedRepository.getById(request.getFeedId());
-        
+
         FeedComment feedComment = FeedComment.of(request.getContent(), feed, member);
         feedCommentRepository.save(feedComment);
 
         saveFeedCommentMedia(request, feedComment);
+
+        return feedComment;
+    }
+
+    @Transactional
+    public FeedComment updateFeed(Long feedCommentId, FeedCommentUpdateRequest request) {
+        if (!StringUtils.hasText(request.getContent())) {
+            throw new FeedCommentException(FeedCommentErrorCode.INVALID_FEED_COMMENT_CONTENT);
+        }
+
+        FeedComment feedComment = feedCommentRepository.getById(feedCommentId);
+
+        // 피드 내용 업데이트
+        feedComment.updateContent(request.getContent());
+
+        // 미디어 업데이트
+        feedCommentMediaService.updateFeedCommentMedia(feedComment, request);
 
         return feedComment;
     }
