2a45a41f8cf7af53fc406c378e15db0ce5d8b3fe
0tae <choiyt3465@naver.com>
2025-06-03 22:01:27 +0900

    feat : 피드 답글 레포지토리 쿼리 메소드 구현 - 피드 답글 ID로 FeedReply 조회

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedreply/repository/FeedReplyRepository.java
@@ -0,0 +1,14 @@
+package com.samsamhajo.deepground.feed.feedreply.repository;
+
+import com.samsamhajo.deepground.feed.feedreply.entity.FeedReply;
+import com.samsamhajo.deepground.feed.feedreply.exception.FeedReplyErrorCode;
+import com.samsamhajo.deepground.feed.feedreply.exception.FeedReplyException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedReplyRepository extends JpaRepository<FeedReply, Long> {
+    default FeedReply getById(Long feedReplyId) {
+        return findById(feedReplyId).orElseThrow(() -> new FeedReplyException(FeedReplyErrorCode.FEED_REPLY_NOT_FOUND));
+    }
+} 
