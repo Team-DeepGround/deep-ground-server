bcc96214522b4101374743a291b9eab997bded6c
0tae <choiyt3465@naver.com>
2025-06-03 22:40:03 +0900

    feat : 피드 답글 좋아요 Repository 쿼리 메소드 정의 - 피드 답글 좋아요 수를 구하기 위한 countByFeedReplyId 정의 - 피드 답글 좋아요 존재 여부를 확인하기 위한 existsByFeedReplyIdAndMemberId 정의 - 피드 답글 좋아요를 가져오기 위한 getByFeedReplyIdAndMemberId 정의

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedreply/repository/FeedReplyLikeRepository.java
@@ -0,0 +1,24 @@
+package com.samsamhajo.deepground.feed.feedreply.repository;
+
+import com.samsamhajo.deepground.feed.feedreply.entity.FeedReplyLike;
+import com.samsamhajo.deepground.feed.feedreply.exception.FeedReplyErrorCode;
+import com.samsamhajo.deepground.feed.feedreply.exception.FeedReplyException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FeedReplyLikeRepository extends JpaRepository<FeedReplyLike, Long> {
+
+    int countByFeedReplyId(Long feedReplyId);
+
+    boolean existsByFeedReplyIdAndMemberId(Long feedReplyId, Long memberId);
+
+    Optional<FeedReplyLike> findByFeedReplyIdAndMemberId(Long feedReplyId, Long memberId);
+
+    default FeedReplyLike getByFeedReplyIdAndMemberId(Long feedReplyId, Long memberId) {
+        return findByFeedReplyIdAndMemberId(feedReplyId, memberId)
+                .orElseThrow(() -> new FeedReplyException(FeedReplyErrorCode.FEED_REPLY_LIKE_NOT_FOUND));
+    }
+} 
