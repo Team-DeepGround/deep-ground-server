8721d97ad1f191c179c4aa29be5ee8561236647c
dotae1 <9636515@gmail.com>
2025-06-04 17:59:16 +0900

    댓글 수정 로직 구현 media 파일 삭제

---
+++ src/main/java/com/samsamhajo/deepground/qna/comment/exception/CommentErrorCode.java
@@ -0,0 +1,22 @@
+package com.samsamhajo.deepground.qna.comment.exception;
+
+import com.samsamhajo.deepground.global.error.core.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum CommentErrorCode implements ErrorCode {
+
+    COMMENT_REQUIRED(HttpStatus.BAD_GATEWAY, "댓글은 필수입니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 없습니다."),
+    COMMENT_ANSWER_MISMATCH(HttpStatus.BAD_REQUEST, "답변에 속한 댓글이 아닙니다."),
+    COMMENT_MEMBER_MISMATCH(HttpStatus.BAD_REQUEST, "댓글을 작성한 멤버가 아닙닌다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    CommentErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
