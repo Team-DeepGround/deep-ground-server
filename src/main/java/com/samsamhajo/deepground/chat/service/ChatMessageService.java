b7c3e942466c9196257b227a08cd46b17a2afa94
조윤성 <yunsung1206@gmail.com>
2025-06-04 18:32:57 +0900

    feat: 채팅 미디어 전송 기능 구현

--- src/main/java/com/samsamhajo/deepground/chat/service/ChatMessageService.java
+++ src/main/java/com/samsamhajo/deepground/chat/service/ChatMessageService.java
@@ -2,11 +2,15 @@ package com.samsamhajo.deepground.chat.service;
 
 import com.samsamhajo.deepground.chat.dto.ChatMessageRequest;
 import com.samsamhajo.deepground.chat.dto.ChatMessageResponse;
+import com.samsamhajo.deepground.chat.entity.ChatMedia;
 import com.samsamhajo.deepground.chat.entity.ChatMessage;
+import com.samsamhajo.deepground.chat.entity.ChatMessageMedia;
 import com.samsamhajo.deepground.chat.exception.ChatMessageErrorCode;
 import com.samsamhajo.deepground.chat.exception.ChatMessageException;
+import com.samsamhajo.deepground.chat.repository.ChatMediaRepository;
 import com.samsamhajo.deepground.chat.repository.ChatMessageRepository;
 import com.samsamhajo.deepground.global.message.MessagePublisher;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,16 +23,18 @@ public class ChatMessageService {
 
     private final MessagePublisher messagePublisher;
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatMediaRepository chatMediaRepository;
 
     @Transactional
     public void sendMessage(Long chatRoomId, Long memberId, ChatMessageRequest request) {
-        if (!StringUtils.hasText(request.getMessage()) && CollectionUtils.isEmpty(request.getMediaIds())) {
+        boolean hasMedia = !CollectionUtils.isEmpty(request.getMediaIds());
+
+        if (!StringUtils.hasText(request.getMessage()) && !hasMedia) {
             throw new ChatMessageException(ChatMessageErrorCode.INVALID_MESSAGE);
         }
 
-        // TODO: 채팅 미디어 관련 로직
-
-        ChatMessage chatMessage = ChatMessage.of(chatRoomId, memberId, request.getMessage());
+        ChatMessage chatMessage = hasMedia ? messageTextAndMedia(chatRoomId, memberId, request)
+                : ChatMessage.of(chatRoomId, memberId, request.getMessage());
         chatMessageRepository.save(chatMessage);
 
         // 채팅 메시지 전송
@@ -36,5 +42,22 @@ public class ChatMessageService {
         messagePublisher.convertAndSend(destination, ChatMessageResponse.from(chatMessage));
 
         // TODO: 읽지 않은 메시지 개수 로직
+    }
+
+    private ChatMessage messageTextAndMedia(Long chatRoomId, Long memberId, ChatMessageRequest request) {
+        List<ChatMedia> chatMedia = chatMediaRepository.findAllByIdAndStatusPending(request.getMediaIds());
+        List<ChatMessageMedia> media = chatMedia.stream()
+                .filter(m -> m.getChatRoomId().equals(chatRoomId) && m.getMemberId().equals(memberId))
+                .map(ChatMedia::getMedia)
+                .toList();
+
+        if (request.getMediaIds().size() != media.size()) {
+            throw new ChatMessageException(ChatMessageErrorCode.MEDIA_NOT_FOUND);
+        }
+
+        chatMedia.forEach(ChatMedia::send);
+        chatMediaRepository.saveAll(chatMedia);
+
+        return ChatMessage.of(chatRoomId, memberId, request.getMessage(), media);
     }
 }
