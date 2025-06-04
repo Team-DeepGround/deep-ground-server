3d1108cb048a912d2f9dd0c77e3156956620670b
dotae1 <9636515@gmail.com>
2025-06-04 17:20:02 +0900

    댓글 수정 로직 구현

---
+++ src/main/java/com/samsamhajo/deepground/qna/comment/exception/CommentException.java
@@ -0,0 +1,7 @@
+package com.samsamhajo.deepground.qna.comment.exception;
+
+import com.samsamhajo.deepground.global.error.core.BaseException;
+
+public class CommentException extends BaseException {
+    public CommentException(CommentErrorCode errorCode) {super(errorCode);}
+}
---
+++ src/main/java/com/samsamhajo/deepground/qna/comment/service/CommentService.java
@@ -0,0 +1,96 @@
+package com.samsamhajo.deepground.qna.comment.service;
+
+import com.samsamhajo.deepground.member.entity.Member;
+import com.samsamhajo.deepground.member.repository.MemberRepository;
+import com.samsamhajo.deepground.qna.answer.entity.Answer;
+import com.samsamhajo.deepground.qna.answer.exception.AnswerErrorCode;
+import com.samsamhajo.deepground.qna.answer.exception.AnswerException;
+import com.samsamhajo.deepground.qna.answer.repository.AnswerRepository;
+import com.samsamhajo.deepground.qna.comment.dto.CommentCreateRequestDto;
+import com.samsamhajo.deepground.qna.comment.dto.CommentCreateResponseDto;
+import com.samsamhajo.deepground.qna.comment.dto.UpdateCommentRequestDto;
+import com.samsamhajo.deepground.qna.comment.dto.UpdateCommentResponseDto;
+import com.samsamhajo.deepground.qna.comment.entity.Comment;
+import com.samsamhajo.deepground.qna.comment.exception.CommentErrorCode;
+import com.samsamhajo.deepground.qna.comment.exception.CommentException;
+import com.samsamhajo.deepground.qna.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final AnswerRepository answerRepository;
+
+    @Transactional
+    public CommentCreateResponseDto createComment(CommentCreateRequestDto commentCreateRequestDto, Long memberId) {
+
+        Member member = memberRepository.findById(memberId).orElseThrow(() ->
+                new IllegalArgumentException("존재하지 않는 사용자입니다.")
+        );
+
+        Answer answer = answerRepository.findById(commentCreateRequestDto.getAnswerId()).orElseThrow(() ->
+                new AnswerException(AnswerErrorCode.ANSWER_NOT_FOUND)
+        );
+
+        if (!StringUtils.hasText(commentCreateRequestDto.getCommentContent())) {
+            throw new CommentException(CommentErrorCode.COMMENT_REQUIRED);
+        }
+
+        Comment comment = Comment.of(
+                commentCreateRequestDto.getCommentContent(),
+                member,
+                answer
+        );
+
+        Comment saved = commentRepository.save(comment);
+
+        answer.incrementCommentCount();
+
+        return CommentCreateResponseDto.of(
+                saved.getCommentContent(),
+                saved.getAnswer().getId(),
+                saved.getMember().getId(),
+                saved.getId()
+        );
+    }
+    @Transactional
+    public UpdateCommentResponseDto updateComment(UpdateCommentRequestDto updateCommentRequestDto, Long memberId) {
+
+        Member member = memberRepository.findById(memberId).orElseThrow(() ->
+                new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        Answer answer = answerRepository.findById(updateCommentRequestDto.getAnswerId()).orElseThrow(() ->
+                new AnswerException(AnswerErrorCode.ANSWER_NOT_FOUND));
+
+        Comment comment = commentRepository.findById(updateCommentRequestDto.getCommentId()).orElseThrow(() ->
+                new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
+
+        if(!StringUtils.hasText(updateCommentRequestDto.getCommentContent())) {
+            throw new CommentException(CommentErrorCode.COMMENT_REQUIRED);
+        }
+
+        if(!comment.getAnswer().getId().equals(updateCommentRequestDto.getAnswerId())) {
+            throw new CommentException(CommentErrorCode.COMMENT_ANSWER_MISMATCH);
+        }
+
+        if(!comment.getMember().getId().equals(memberId)) {
+            throw new CommentException(CommentErrorCode.COMMENT_MEMBER_MISMATCH);
+        }
+        comment.updateCommentContent(updateCommentRequestDto.getCommentContent());
+
+        return UpdateCommentResponseDto.of(
+                comment.getCommentContent(),
+                comment.getMember().getId(),
+                comment.getAnswer().getId(),
+                comment.getId()
+
+        );
+
+    }
+}
