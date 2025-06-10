0854a43ef74926cd3c68bd45f7207352ec851cc6
0tae <choiyt3465@naver.com>
2025-06-10 21:50:13 +0900

    feat : 피드 답글 목록 불러오기 응답 Dto 클래스 정의

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedreply/model/FetchFeedReplyResponse.java
@@ -0,0 +1,20 @@
+package com.samsamhajo.deepground.feed.feedreply.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+public class FetchFeedReplyResponse {
+    private long memberId;
+    private long feedReplyId;
+    private String memberName;
+    private String content;
+    private int likeCount;
+    private int profileImageId;
+    private LocalDate createdAt;
+    private List<Long> mediaIds;
+} 
