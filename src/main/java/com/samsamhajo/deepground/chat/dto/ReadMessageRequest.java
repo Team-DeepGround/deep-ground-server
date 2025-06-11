119c15fc1481eba5ddbc39b5ba2af11f75d11a4c
조윤성 <yunsung1206@gmail.com>
2025-06-12 03:51:29 +0900

    refactor: ReadMessageRequest 기본 생성자 추가

--- src/main/java/com/samsamhajo/deepground/chat/dto/ReadMessageRequest.java
+++ src/main/java/com/samsamhajo/deepground/chat/dto/ReadMessageRequest.java
@@ -1,10 +1,13 @@
 package com.samsamhajo.deepground.chat.dto;
 
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ReadMessageRequest {
 
