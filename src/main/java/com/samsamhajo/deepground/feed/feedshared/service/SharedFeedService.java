83275548f953df594350d964bd8d692744331a9d
0tae <choiyt3465@naver.com>
2025-06-11 17:42:29 +0900

    feat : 피드 공유 생성하기 서비스 로직 구현

---
+++ src/main/java/com/samsamhajo/deepground/feed/feedshared/service/SharedFeedService.java
@@ -0,0 +1,39 @@
+package com.samsamhajo.deepground.feed.feedshared.service;
+
+import com.samsamhajo.deepground.feed.feed.entity.Feed;
+import com.samsamhajo.deepground.feed.feed.repository.FeedRepository;
+import com.samsamhajo.deepground.feed.feedshared.dto.SharedFeedRequest;
+import com.samsamhajo.deepground.feed.feedshared.entity.SharedFeed;
+import com.samsamhajo.deepground.feed.feedshared.repository.SharedFeedRepository;
+import com.samsamhajo.deepground.member.entity.Member;
+import com.samsamhajo.deepground.member.exception.MemberErrorCode;
+import com.samsamhajo.deepground.member.exception.MemberException;
+import com.samsamhajo.deepground.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SharedFeedService {
+
+    private final SharedFeedRepository sharedFeedRepository;
+    private final FeedRepository feedRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public SharedFeed createSharedFeed(SharedFeedRequest request, Long memberId) {
+        Feed originFeed = feedRepository.getById(request.getOriginFeedId());
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.INVALID_MEMBER_ID));
+
+        Feed feed = Feed.of(request.getContent(), member);
+        feedRepository.save(feed);
+
+        SharedFeed sharedFeed = SharedFeed.of(feed, originFeed, member);
+        sharedFeedRepository.save(sharedFeed);
+        
+        return sharedFeed;
+    }
+} 
