773227f5c78b0922f8c9cc4ba42c16281d6286b1
0tae <choiyt3465@naver.com>
2025-06-11 20:09:46 +0900

    refactor : Lombok Constructor 추가

--- src/main/java/com/samsamhajo/deepground/feed/feedshared/dto/SharedFeedRequest.java
+++ src/main/java/com/samsamhajo/deepground/feed/feedshared/dto/SharedFeedRequest.java
@@ -1,9 +1,13 @@
 package com.samsamhajo.deepground.feed.feedshared.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SharedFeedRequest {
     @NotNull(message = "원본 피드 ID는 필수입니다.")
     private Long originFeedId;
