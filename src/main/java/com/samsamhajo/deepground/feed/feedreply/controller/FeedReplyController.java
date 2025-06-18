cbaa283384dc814f7681d768508fb062e512ac3d
0tae <choiyt3465@naver.com>
2025-06-18 23:00:28 +0900

    refactor : 피드 답글 코드 리팩토링 - @AuthenticationPrincipal 어노테이션 사용 - DEV_MEMBER_ID 제거 - 서비스 로직에서 MemberRepository 의존성 제거 및 Member를 findById로 가져오는 로직 제거 - 일부 서비스 구현부 메서드 파라미터 수정 Long -> Member

--- src/main/java/com/samsamhajo/deepground/feed/feedreply/controller/FeedReplyController.java
+++ src/main/java/com/samsamhajo/deepground/feed/feedreply/controller/FeedReplyController.java
@@ -1,5 +1,6 @@
 package com.samsamhajo.deepground.feed.feedreply.controller;
 
+import com.samsamhajo.deepground.auth.security.CustomUserDetails;
 import com.samsamhajo.deepground.feed.feedreply.exception.FeedReplySuccessCode;
 import com.samsamhajo.deepground.feed.feedreply.model.FeedReplyCreateRequest;
 import com.samsamhajo.deepground.feed.feedreply.model.FeedReplyUpdateRequest;
@@ -9,6 +10,7 @@ import com.samsamhajo.deepground.global.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,13 +19,13 @@ import org.springframework.web.bind.annotation.*;
 public class FeedReplyController {
 
     private final FeedReplyService feedReplyService;
-    private final Long DEV_MEMBER_ID = 1L;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<SuccessResponse<?>> createFeedReply(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @ModelAttribute FeedReplyCreateRequest request) {
 
-        feedReplyService.createFeedReply(request, DEV_MEMBER_ID);
+        feedReplyService.createFeedReply(request, userDetails.getMember());
 
         return ResponseEntity
                 .ok(SuccessResponse.of(FeedReplySuccessCode.FEED_REPLY_CREATED));
@@ -52,9 +54,11 @@ public class FeedReplyController {
 
     @GetMapping("/list/{feedReplyId}")
     public ResponseEntity<SuccessResponse<?>> getFeedReplies(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable("feedReplyId") Long feedCommentId) {
 
-        FetchFeedRepliesResponse response = feedReplyService.getFeedReplies(feedCommentId, DEV_MEMBER_ID);
+        FetchFeedRepliesResponse response =
+                feedReplyService.getFeedReplies(feedCommentId, userDetails.getMember().getId());
 
         return ResponseEntity
                 .ok(SuccessResponse.of(FeedReplySuccessCode.FEED_REPLY_LIST_FETCHED, response));
