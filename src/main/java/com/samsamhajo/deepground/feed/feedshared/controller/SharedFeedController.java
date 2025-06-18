589afa07c6417de78ac309ed0887fdb3aa5b427c
0tae <choiyt3465@naver.com>
2025-06-18 23:00:40 +0900

    refactor : 피드 공유 코드 리팩토링 - @AuthenticationPrincipal 어노테이션 사용 - DEV_MEMBER_ID 제거 - 서비스 로직에서 MemberRepository 의존성 제거 및 Member를 findById로 가져오는 로직 제거 - 일부 서비스 구현부 메서드 파라미터 수정 Long -> Member

--- src/main/java/com/samsamhajo/deepground/feed/feedshared/controller/SharedFeedController.java
+++ src/main/java/com/samsamhajo/deepground/feed/feedshared/controller/SharedFeedController.java
@@ -1,5 +1,6 @@
 package com.samsamhajo.deepground.feed.feedshared.controller;
 
+import com.samsamhajo.deepground.auth.security.CustomUserDetails;
 import com.samsamhajo.deepground.feed.feedshared.dto.SharedFeedRequest;
 import com.samsamhajo.deepground.feed.feedshared.exception.SharedFeedSuccessCode;
 import com.samsamhajo.deepground.feed.feedshared.service.SharedFeedService;
@@ -7,6 +8,7 @@ import com.samsamhajo.deepground.global.success.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class SharedFeedController {
 
     private final SharedFeedService sharedFeedService;
-    private final Long DEV_MEMBER_ID = 1L;
     @PostMapping
     public ResponseEntity<SuccessResponse<?>> createSharedFeed(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody SharedFeedRequest request) {
 
-        sharedFeedService.createSharedFeed(request, DEV_MEMBER_ID);
+        sharedFeedService.createSharedFeed(request, userDetails.getMember());
         
         return ResponseEntity.ok(SuccessResponse.of(SharedFeedSuccessCode.FEED_SHARED));
     }
