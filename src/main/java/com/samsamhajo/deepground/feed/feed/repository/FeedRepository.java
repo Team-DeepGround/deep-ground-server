a404b4994e52ba779910ade2d151661c91aef322
0-tae <choiyt3465@naver.com>
2025-07-02 01:57:45 +0900

    feat : findAllByMemberId 구현

--- src/main/java/com/samsamhajo/deepground/feed/feed/repository/FeedRepository.java
+++ src/main/java/com/samsamhajo/deepground/feed/feed/repository/FeedRepository.java
@@ -3,6 +3,8 @@ package com.samsamhajo.deepground.feed.feed.repository;
 import com.samsamhajo.deepground.feed.feed.entity.Feed;
 import com.samsamhajo.deepground.feed.feed.exception.FeedErrorCode;
 import com.samsamhajo.deepground.feed.feed.exception.FeedException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,6 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     default Feed getById(Long id) {
         return findById(id).orElseThrow(() -> new FeedException(FeedErrorCode.FEED_NOT_FOUND));
     }
+
+    Page<Feed> findAllByMemberId(Pageable pageable, Long memberId);
 }
