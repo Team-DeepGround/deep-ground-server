5840ca9ef8d1142688a572e3909acffcd4282c89
조윤성 <yunsung1206@gmail.com>
2025-07-03 17:28:46 +0900

    feat: 스케줄 생성 알림 전송

--- src/main/java/com/samsamhajo/deepground/notification/entity/NotificationType.java
+++ src/main/java/com/samsamhajo/deepground/notification/entity/NotificationType.java
@@ -1,11 +1,23 @@
 package com.samsamhajo.deepground.notification.entity;
 
 public enum NotificationType {
+
+    /**
+     * 친구 알림
+     */
     FRIEND_REQUEST,         // 친구 요청
     FRIEND_ACCEPT,          // 친구 요청 수락
+
+    /**
+     * 스터디 그룹 알림
+     */
     STUDY_GROUP_KICK,       // 스터디 그룹 강퇴
     STUDY_GROUP_JOIN,       // 스터디 그룹 가입 요청
     STUDY_GROUP_ACCEPT,     // 스터디 그룹 가입 성공
+
+    /**
+     * 스케줄 알림
+     */
     SCHEDULE_CREATE,        // 스터디 일정 생성
     SCHEDULE_REMINDER,      // 스터디 일정 알림
 
