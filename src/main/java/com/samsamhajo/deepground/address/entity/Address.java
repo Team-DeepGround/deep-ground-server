4c4ff5b41b9408f13d17df1c083c4cf948671b96
paka <wjd1357912@gmail.com>
2025-07-18 17:38:32 +0900

    feat : address 엔티티 추가

---
+++ src/main/java/com/samsamhajo/deepground/address/entity/Address.java
@@ -0,0 +1,43 @@
+package com.samsamhajo.deepground.address.entity;
+
+
+import com.samsamhajo.deepground.qna.answer.dto.AnswerDetailDto;
+import com.samsamhajo.deepground.studyGroup.entity.StudyGroupAddress;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "gu")
+    private String gu;
+
+    @Column(name = "dong")
+    private String dong;
+
+    @OneToMany
+    @JoinColumn(name = "address_id")
+    private List<StudyGroupAddress> studyGroupAddress = new ArrayList<>();
+
+    private Address (String city, String gu, String dong){
+        this.city =city;
+        this.gu = gu;
+        this.dong = dong;
+    }
+
+}
