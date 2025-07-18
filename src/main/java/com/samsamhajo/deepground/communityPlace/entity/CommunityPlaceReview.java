f2c9b0074cc669fbf9fc0014e407087db63d418c
paka <wjd1357912@gmail.com>
2025-07-18 17:38:59 +0900

    feat: communityPlaceReview 엔티티 추가

---
+++ src/main/java/com/samsamhajo/deepground/communityPlace/entity/CommunityPlaceReview.java
@@ -0,0 +1,29 @@
+package com.samsamhajo.deepground.communityPlace.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "Community_place_reviews)")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommunityPlaceReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "communtiy_place_reviews")
+    private Long id;
+
+    @Column(name = "community_palce_scope")
+    private double scope;
+
+    @Column(name = "community_place_content")
+    private String content;
+
+    private CommunityPlaceReview(double scope,String content){
+        this.scope =scope;
+        this.content = content;
+    }
+}
