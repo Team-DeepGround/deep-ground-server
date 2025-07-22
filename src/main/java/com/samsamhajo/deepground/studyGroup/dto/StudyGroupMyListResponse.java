320b425368622e5beb19223c340b72b93c4440ad
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-07-22 09:53:18 +0900

    fe가t: StudyGroupMyListResponse에 필요한 필드 추

--- src/main/java/com/samsamhajo/deepground/studyGroup/dto/StudyGroupMyListResponse.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/dto/StudyGroupMyListResponse.java
@@ -16,13 +16,21 @@ public class StudyGroupMyListResponse {
   private String title;
   private LocalDate createdAt;
   private GroupStatus groupStatus;
+  private Integer groupMemberCount;
+  private Integer currentMemberCount;
+  private LocalDate studyStartDate;
+  private LocalDate studyEndDate;
 
   public static StudyGroupMyListResponse from(StudyGroup group) {
     return StudyGroupMyListResponse.builder()
-        .id(group.getId())
-        .title(group.getTitle())
-        .createdAt(group.getCreatedAt().toLocalDate()) // BaseEntity 기반
-        .groupStatus(group.getGroupStatus())
-        .build();
+            .id(group.getId())
+            .title(group.getTitle())
+            .createdAt(group.getCreatedAt().toLocalDate()) // BaseEntity 기반
+            .groupStatus(group.getGroupStatus())
+            .groupMemberCount(group.getGroupMemberCount())
+            .currentMemberCount(group.getMembers().size())
+            .studyStartDate(group.getStudyStartDate())
+            .studyEndDate(group.getStudyEndDate())
+            .build();
   }
 }
