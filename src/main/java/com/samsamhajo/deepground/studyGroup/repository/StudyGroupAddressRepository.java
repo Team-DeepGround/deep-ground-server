39e71c45c6f4739e92279381830b9efa257181d3
0tae <choiyt3465@naver.com>
2025-07-24 17:56:14 +0900

    feat : 스터디 그룹 지역별 집계 조회 쿼리 작성

--- src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupAddressRepository.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupAddressRepository.java
@@ -1,7 +1,19 @@
 package com.samsamhajo.deepground.studyGroup.repository;
 
+import com.samsamhajo.deepground.studyGroup.dto.CalculatedStudyGroupsInLocalResultDto;
 import com.samsamhajo.deepground.studyGroup.entity.StudyGroupAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudyGroupAddressRepository extends JpaRepository<StudyGroupAddress, Long> {
+    @Query(value = """
+        SELECT COUNT(studyGroupAddress.id), studyGroupAddress.studyGroup.id
+        FROM StudyGroupAddress studyGroupAddress
+        WHERE studyGroupAddress.address.id IN :addressIds
+        GROUP BY studyGroupAddress.address.id
+    """)
+    List<CalculatedStudyGroupsInLocalResultDto> countStudyGroupByAddressIdsGroupByAddressId(@Param("addressIds") List<Long> addressIds);
 }
