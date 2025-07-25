5c773bdadab7aa9bcd188c03453a52894d58fa29
sseen2 <kse10023@naver.com>
2025-07-26 00:38:02 +0900

    feat: 지역별 스터디 그룹 리스트 조회 쿼리 작성

--- src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupAddressRepository.java
+++ src/main/java/com/samsamhajo/deepground/studyGroup/repository/StudyGroupAddressRepository.java
@@ -2,6 +2,18 @@ package com.samsamhajo.deepground.studyGroup.repository;
 
 import com.samsamhajo.deepground.studyGroup.entity.StudyGroupAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudyGroupAddressRepository extends JpaRepository<StudyGroupAddress, Long> {
+
+    @Query("""
+        SELECT s.studyGroup.id
+        FROM StudyGroupAddress s
+        WHERE s.address.city = :city
+          AND s.address.gu = :gu
+          AND s.address.dong = :dong
+    """)
+    List<Long> findStudyGroupIdByAddress(String city, String gu, String dong);
 }
