87aeda1a7bae5d273f61bb80dc572a8511ae07e3
paka <wjd1357912@gmail.com>
2025-07-25 23:06:07 +0900

    feat: 사용자 정의 쿼리 메서드 작성

--- src/main/java/com/samsamhajo/deepground/communityPlace/repository/SpecificAddressRepository.java
+++ src/main/java/com/samsamhajo/deepground/communityPlace/repository/SpecificAddressRepository.java
@@ -1,12 +1,15 @@
 package com.samsamhajo.deepground.communityPlace.repository;
 
+import com.samsamhajo.deepground.communityPlace.dto.CommunityPlaceReviewDto;
 import com.samsamhajo.deepground.communityPlace.entity.CommunityPlaceReview;
 import com.samsamhajo.deepground.communityPlace.entity.SpecificAddress;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +20,17 @@ public interface SpecificAddressRepository extends JpaRepository<SpecificAddress
             "WHERE sa.id = :specificAddressId")
     Optional<CommunityPlaceReview> findByIdCountReviewsAndScopeAverage(@Param("specificAddressId") Long specificAddressId);
 
+    @Query("SELECT sa.name, sa.number " +
+            "FROM SpecificAddress sa " +
+            "LEFT JOIN sa.communityPlaceReviews r " +
+            "GROUP BY sa.name, sa.number " +
+            "ORDER BY COUNT(r) DESC")
+    List<CommunityPlaceReviewDto> findAllCommunityPlaceByReviewCountDesc();
+
+    @Query("SELECT sa.name, sa.number " +
+            "FROM SpecificAddress sa " +
+            "LEFT JOIN sa.communityPlaceReviews r " +
+            "GROUP BY sa.name, sa.number " +
+            "ORDER BY AVG(r.scope) DESC")
+    List<CommunityPlaceReviewDto> findAllCommunityPlaceByReviewScopeDesc();
 }
