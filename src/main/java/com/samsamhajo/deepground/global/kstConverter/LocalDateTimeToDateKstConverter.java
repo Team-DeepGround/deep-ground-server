5f1c7fecd3a7eef8a0e1f43dc5bab5947dda81f6
paka <wjd1357912@gmail.com>
2025-10-28 16:43:16 +0900

    fix: 중복된 코드 수정

--- src/main/java/com/samsamhajo/deepground/global/kstConverter/LocalDateTimeToDateKstConverter.java
+++ src/main/java/com/samsamhajo/deepground/global/kstConverter/LocalDateTimeToDateKstConverter.java
@@ -4,23 +4,24 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
 @Component
 @ReadingConverter
-public class LocalDateTimeToDateKstConverter implements Converter<Date, LocalDateTime> {
+public class LocalDateTimeToDateKstConverter implements Converter<LocalDateTime,Date> {
 
     private static final int KST_OFFSET_HOURS = 9;
 
     @Override
-    public LocalDateTime convert(Date source) {
+    public Date convert(LocalDateTime source) {
         return convertToKst(source);
     }
 
-    private LocalDateTime convertToKst(Date date){
-        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        return localDateTime.minusHours(KST_OFFSET_HOURS);
+    private Date convertToKst(LocalDateTime localDateTime){
+        return Timestamp.valueOf(localDateTime.plusHours(KST_OFFSET_HOURS));
     }
 }
+
