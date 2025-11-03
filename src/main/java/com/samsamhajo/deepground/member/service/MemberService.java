5db567d1c5373c046a6d48770bd37b34f0531546
seanjung0915@gmail.com <seanjung0915@gmail.com>
2025-11-03 23:43:51 +0900

    refactor: 프로필 관련 수정

--- src/main/java/com/samsamhajo/deepground/member/service/MemberService.java
+++ src/main/java/com/samsamhajo/deepground/member/service/MemberService.java
@@ -118,7 +118,7 @@ public class MemberService {
 
     // TODO: 비공개 프로필 설정 도입 시, memberId와 profileId를 비교하여 접근 제한 예외 처리 추가 예정
     @Transactional(readOnly = true)
-    public MemberProfileDto getUserProfile(Long memberId, UUID profilePublicId) {
+    public MemberProfileDto getUserProfile(UUID profilePublicId) {
 
         MemberProfile profile = profileRepository.findByProfilePublicId(profilePublicId)
                 .orElseThrow(()-> new ProfileException(ProfileErrorCode.INVALID_PROFILE_ID));
